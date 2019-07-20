60dbd411c51aa89604275f459eb823952ad0037d
llaor <projectellaor@gmail.com>
2019-07-20 12:00:49 +0200

    First release

---
+++ precache-manifest.bbb5fe6547375499b61972c117bb3492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2efb68036d4f4156c2a0a4a497d4ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87bcba22c602250a12f6",
+    "url": "/static/css/main.680cce99.chunk.css"
+  },
+  {
+    "revision": "03f4d2d401925231fdd9",
+    "url": "/static/js/2.12fb7cd8.chunk.js"
+  },
+  {
+    "revision": "87bcba22c602250a12f6",
+    "url": "/static/js/main.8aa5eef0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
